1c1a5a88a71dccc1d0a48153e05db389f52800b0
Roberto Marques <robertomatheo@usp.br>
2024-02-05 18:14:41 -0300

    Selecionar cada instituto da USP - mestrado

--- prototype.py
+++ prototype.py
@@ -20,26 +20,51 @@ def run():
             
             fom  = driver.find_element(By.CLASS_NAME, 'dropdown')
             fom.click()
-            time.sleep(1)
+            time.sleep(2)
             #abre o item de fomento à pesquisa
             
             pg = driver.find_element(By.LINK_TEXT, programa)
             pg.click()
-            time.sleep(2.5)
+            time.sleep(3.5)
             #abre a página de cada programa
 
             inst = driver.find_element(By.ID, 'pivot_expand_instfacet')
             inst.click()
-            time.sleep(4)
+            time.sleep(4.5)
             #abre instituições sede
 
             a1 = driver.find_element(By.XPATH, '//li[@id="pivot_expand_Universidades"]/a')
             a1.click()
-            time.sleep(2.5)
+            time.sleep(3.5)
             a2 = driver.find_element(By.XPATH, '//li[@id="pivot_expand_UniversidadedeSoPauloUSP"]/a')
             a2.click()
-            time.sleep(1.5)
+            time.sleep(2.5)
             #abre as instuituiçoes de ensino da usp
+
+            unidusp = driver.find_elements(By.XPATH, 
+                '/html/body/div[3]/div/div[2]/div/div[2]/section[2]/div[1]/form/ul/div[4]/ul/div/li/ul/li[10]/ul/li[67]/ul/li/input')
+            time.sleep(3)
+
+            for unidade in unidusp:
+                unidade.click()
+                time.sleep(1)
+                unidade.click()
+                time.sleep(1)
+
+            ######ALGORITMO DE ABRE E FECHA
+            '''
+            cebi = driver.find_element(By.XPATH, 
+                '//*[@id="34_checkbox_UniversidadesUniversidadedeSoPauloUSPCentrodeBiologiaMarinhaCEBIMAR"]')
+            cebi.click()
+            time.sleep(1)
+            cebi.click()
+            time.sleep(1)
+            cena = driver.find_element(By.XPATH, 
+                '//*[@id="34_checkbox_UniversidadesUniversidadedeSoPauloUSPCentrodeEnergiaNuclearnaAgriculturaCENA"]')
+            time.sleep(1)
+            cena.click()
+            '''
+            ############ FIM DO ALGORITMO
             '''
             unid = driver.find_element(By.XPATH, '//body/div[3]/div/div[2]/div/div[2]/section[2]/div[2]/form/ul/div[4]/ul/div/li/ul/li[10]/ul/li[67]/ul/li[1]/input')
             unid.click()
@@ -81,3 +106,4 @@ def run():
 
 if __name__ == '__main__':
     run()
+
