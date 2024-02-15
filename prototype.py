82eed420d5e04ab344f80faf994b3448379600e7
Roberto Marques <robertomatheo@usp.br>
2024-02-15 17:33:39 -0300

    Gerar planilha completa e corrigida

--- prototype.py
+++ prototype.py
@@ -6,27 +6,26 @@ import time
 import openpyxl
 import os
 
+from objectsprototype import *
+
 #https://openpyxl.readthedocs.io/en/stable/api/openpyxl.worksheet.worksheet.html  documentação openpyxl
 
-def run():
+dominio = 'https://bv.fapesp.br/pt/'
+
+def mestrado():
 #função  principal
 
-
-
     driver = webdriver.Firefox()
-    driver.get('https://bv.fapesp.br/pt/')
+    driver.get(dominio)
     driver.maximize_window()
     time.sleep(2)
     #abre o site da biblioteca virtual
 
-    programas = ['Mestrado', 'Doutorado', 'Doutorado Direto', 'Pós-Doutorado']
-
-    ###MESTRADO
-
     m = 1
+    pagina = PáginaFapesp(driver)
     
-    fom = WebDriverWait(driver,15).until(EC.presence_of_element_located((By.CLASS_NAME, 'dropdown')))
-    fom.click()
+    fomento = WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.CLASS_NAME, 'dropdown')))
+    fomento.click()
     #abre o item de fomento à pesquisa
     
     pg = WebDriverWait(driver,15).until(EC.presence_of_element_located((By.LINK_TEXT, 'Mestrado')))
@@ -59,6 +58,7 @@ def run():
     ni = WebDriverWait(driver,15).until(EC.presence_of_element_located((
         By.XPATH, '//*[@id="conteudo"]/div[2]/div/div[1]/section/div[1]/span')))
     nome_instituto.append(ni.text[19:])
+    len_anterior = len(ni.text[19:])
     retangulo = WebDriverWait(driver,10).until(EC.presence_of_element_located((By.XPATH, '//body/div[3]/div/div[6]/section/div/div[1]/*[name()="svg" and @class = "svg-content"]/*[name()="g"]/*[name()="g"][16]/*[local-name()="rect"][2]')))
     time.sleep(1.5)
     verifica_nulo = retangulo.get_attribute('height')
@@ -69,40 +69,34 @@ def run():
         time.sleep(2)
         num_bolsas = driver.find_element(By.XPATH, '//body/div[6]/span')
         text_bolsas = num_bolsas.text
-    num_de_bolsas.append(text_bolsas[8:])
+    num_temp = int(text_bolsas[8:])
+    num_de_bolsas.append(num_temp)
     #pega nome e numero de bolsas da primeira unidade
 
 
     while m != 58: # itera sobre todas as unidades
         
-        time.sleep(2)
-        unid = WebDriverWait(driver,10).until(EC.presence_of_element_located((By.XPATH,
-            '//li[@id = "pivot_expand_UniversidadedeSoPauloUSP"]/ul/li[{}]/input'.format(m))))
-        #//*[@id="34_checkbox_UniversidadesUniversidadedeSoPauloUSPEscoladeEngenhariadeLorenaEEL"]
-        #//*[@id="pivot_expand_UniversidadedeSoPauloUSP"]
-        #/html/body/div[3]/div/div[2]/div/div[2]/section[2]/div[2]/form/ul/div[4]/ul/div/li/ul/li[10]/ul/li[67]
-        time.sleep(3)
-        unid.click()
-        
         m += 1
         time.sleep(3)
+        somatorio = num_temp
         
-        unid = WebDriverWait(driver,10).until(EC.presence_of_element_located((By.XPATH,
+        unid = WebDriverWait(driver,15).until(EC.presence_of_element_located((By.XPATH,
             '//li[@id = "pivot_expand_UniversidadedeSoPauloUSP"]/ul/li[{}]/input'.format(m))))
         time.sleep(1)
         unid.click()
 
-        refino = WebDriverWait(driver,10).until(EC.presence_of_element_located((By.XPATH,
+        refino = WebDriverWait(driver,15).until(EC.presence_of_element_located((By.XPATH,
             '//div[@id="refinamento_instfacet"]/li[2]/input')))
         time.sleep(1)
         refino.click()
         time.sleep(1)
 
-        ni = WebDriverWait(driver,10).until(
+        ni = WebDriverWait(driver,15).until(
             EC.presence_of_element_located((By.XPATH, '//*[@id="conteudo"]/div[2]/div/div[1]/section/div[1]/span')))
-        nome_instituto.append(ni.text[19:])
+        nome_instituto.append(ni.text[len_anterior:])
+        len_anterior = len(ni.text)
         
-        retangulo = WebDriverWait(driver,10).until(
+        retangulo = WebDriverWait(driver,15).until(
             EC.presence_of_element_located((By.XPATH, '//body/div[3]/div/div[6]/section/div/div[1]/*[name()="svg" and @class = "svg-content"]/*[name()="g"]/*[name()="g"][34]/*[local-name()="rect"][2]')))
         time.sleep(1.5)
         verifica_nulo = retangulo.get_attribute('height')
@@ -115,7 +109,12 @@ def run():
                 EC.presence_of_element_located((By.XPATH, '//body/div[@class = "d3-tip n"]/span')))
             text_bolsas = num_bolsas.text
             time.sleep(1)
-        num_de_bolsas.append(text_bolsas[8:])
+        num_temp = int(text_bolsas[8:])
+
+        num_final = num_temp - somatorio    
+        num_de_bolsas.append(num_final)
+        print(nome_instituto)
+        print(num_de_bolsas)
 
 
     planilha = openpyxl.Workbook()
@@ -131,47 +130,15 @@ def run():
 
     j = 2
     for elemento in num_de_bolsas:
-        sheet['B{}'.format(j)] = int(elemento)
+        sheet['B{}'.format(j)] = elemento
         j += 1
 
     planilha.save('Bolsas Fapesp_final.xlsx')
 
-
-
-
-
-
-
-
-def planilhar_dados():
-#rascunho do recurso de pegar numero de bolsas e colocar em planilha
-    
-    with webdriver.Chrome() as driver:
-        driver.get('https://bv.fapesp.br/pt/')
-        time.sleep(3)
-
-        navbar = driver.find_element(By.XPATH, '//*[@id="navbarNavDropdown"]/ul')
-
-        texto = navbar.find_elements(By.XPATH, '//li/a/b')
-        
-        lista = []
-
-        for elemento in texto:
-            lista.append(elemento.text)
-            time.sleep(1)
-        
-        planilha = openpyxl.Workbook()
-        sheet = planilha.active
-
-        i = 1
-        for elemento in lista:
-            sheet['A{}'.format(i)] = elemento
-            i += 1
-
-        planilha.save('hello.xlsx')
+    #escrever na planilha: criar workbook, criar planilha e salvar depois da escrita
 
         
 if __name__ == '__main__':
-    run()
+    mestrado()
    
 
